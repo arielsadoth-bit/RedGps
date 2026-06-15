49601c92085a179848a2ef4ac1969afcff944a22
arel sadoth <arielsadoth@gmail.com>
2026-06-15 12:38:54 -0600

    Quitar despliegue fijo y dejar tunel temporal

--- configurar-url-fija.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "%~dp0"
-powershell -NoExit -NoProfile -ExecutionPolicy Bypass -File "%~dp0configurar-url-fija.ps1"
--- iniciar-url-fija.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "%~dp0"
-powershell -NoExit -NoProfile -ExecutionPolicy Bypass -File "%~dp0iniciar-url-fija.ps1"
